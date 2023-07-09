b231635c6d91663323b12c6d7071d17b263b88f6
Ryan Goodwin <goodwinryan2@gmail.com>
2023-07-09 17:15:17 -0400

    Get script running

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
 import time
-
 import requests
-from threading import Thread
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.webdriver import WebDriver
@@ -13,7 +12,7 @@ def get_live_video_urls(instance_url="https://jupiter.tube/"):
     :param instance_url: base url of the PeerTube instance that will be called
     :return: list of URLs as strings
     """
-    search_url: str = f"{instance_url}api/v1/search/videos?isLive=false&sort=-publishedAt"
+    search_url: str = f"{instance_url}api/v1/search/videos?isLive=true&sort=-publishedAt"
     videos_json = requests.get(search_url).json()
     return [v["url"] for v in videos_json["data"]]
 
@@ -42,22 +41,28 @@ def manage_threads(instances, live_video_urls: list[str]) -> None:
             thread[1].start()
 
 
-def main(url: str, timeout_sec: int = 300) -> None:
-    try:
-        browser_instances = []
-        while True:
-            live_videos = get_live_video_urls(url)
-            for video in live_videos:
+def main(url: str, timeout_sec: int) -> None:
+    browser_instances = []
+    while True:
+        print("Checking for live videos")
+        live_videos = get_live_video_urls(url)
+
+        print(f"Current live videos: {len(live_videos)}")
+        for (url, browser) in browser_instances:
+            if url not in live_videos:
+                print(f"{url} is no longer live. Closing browser session")
+                browser.close()
+
+        for video in live_videos:
+            if video not in (i[0] for i in browser_instances):
                 browser = run_browser_instance(video)
                 browser_instances.append((video, browser))
 
-            print(browser_instances)
-
-            # This will probably be made better by scheduling the function calls
-            time.sleep(timeout_sec)
-    except KeyboardInterrupt:
-        quit()
+        # This will probably be made better by scheduling the function calls
+        time.sleep(timeout_sec)
 
 
 if __name__ == '__main__':
-    main("https://jupiter.tube/", timeout_sec=5)
+    api_url = os.getenv("peertube_url", default="https://jupiter.tube/")
+    timeout = os.getenv("retry_sec", default="300")
+    main(api_url, timeout_sec=int(timeout))
